6e76c8f39186ae7207ee4c25ce84449511cf7f35
Christian Bayer <christian@oxigennio.com.br>
2016-11-17 09:57:43 -0200

    checkout

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -44,10 +44,31 @@ angular.module('starter', ['ionic', 'starter.controllers', 'angular-oauth2'])
                 url: '/login',
                 templateUrl: 'templates/login.html',
                 controller: 'LoginCtrl'
-            }).state('home', {
+            })
+            .state('home', {
                 url: '/home',
                 templateUrl: 'templates/home.html',
                 controller: function($scope){}
+            })
+            .state('client', {
+                abstract: true,
+                url: '/client',
+                template: '<ui-view/>'
+            })
+            .state('client.checkout',{
+                url: '/checkout',
+                templateUrl: 'templates/client/checkout.html',
+                controller: 'ClientCheckoutCtrl'
+            })
+            .state('client.checkout_item_detail',{
+                url: '/checkout/detail/:index',
+                templateUrl: 'templates/client/checkout_item_detail.html',
+                controller: 'ClientCheckoutDetailCtrl'
+            })
+            .state('client.view_products',{
+                url: '/view_products',
+                templateUrl: 'templates/client/view_products.html',
+                controller: 'ClientViewProductCtrl'
             });
 
         // $urlRouterProvider.otherwise('/');
